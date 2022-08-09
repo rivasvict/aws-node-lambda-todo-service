5dab7624a2c6326889a0db761ca8adb91386728a
Victor Rivas <victor_rivas@epam.com>
2022-08-09 09:06:48 -0400

    Improved the file structure

--- src/hello.js
+++ src/hello.js
@@ -1,6 +1,6 @@
 "use strict";
 
-module.exports.hello = async (event) => {
+const hello = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify(
@@ -12,4 +12,8 @@ module.exports.hello = async (event) => {
       2
     ),
   };
+};
+
+module.exports = {
+  handler: hello
 };
