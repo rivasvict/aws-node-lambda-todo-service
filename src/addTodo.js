e39309902fa2fea66e2468d8db5a7bb745d59ad7
Victor Rivas <victor_rivas@epam.com>
2022-08-09 10:52:34 -0400

    Added a new to-do function

---
+++ src/addTodo.js
@@ -0,0 +1,46 @@
+"use strict";
+// uuid library is a library to generate unique ids
+const { v4 } = require("uuid");
+const AWS = require("aws-sdk");
+
+const addTodo = async (event) => {
+  /**
+   * REMINDER, for this to work your AWS user for
+   * serverless should get access to be able to
+   * use the dynamoDb resource.
+   */
+  const dynamo = AWS.DynamoDB.DocumentClient()
+
+  /**
+   * The event.body (where the body params
+   * of the request live) is a string
+   * instead of an object so we need to JSON
+   * parse it.
+   */
+  const { todo } = JSON.parse(event.body);
+  const createdAt = new Date();
+  // Generate a unique id
+  const id = v4();
+  console.log('This is an id', id);
+
+  const newTodo = {
+    id,
+    todo,
+    createdAt,
+    completed: false
+  }
+
+  await dynamo.put({
+    TableName: "TodoTable",
+    Item: newTodo
+  }).promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(newTodo),
+  };
+};
+
+module.exports = {
+  handler: addTodo,
+};
