0f625ed88bb92c957097683fa364cb12375f9ce8
Victor Rivas <victor_rivas@epam.com>
2022-08-09 12:27:42 -0400

    Added the json parser middleware and the dependency injection working

--- src/addTodo.js
+++ src/addTodo.js
@@ -2,14 +2,22 @@
 // uuid library is a library to generate unique ids
 const { v4 } = require("uuid");
 const AWS = require("aws-sdk");
+/**
+ * This middy Pachage helps to parse the body of the event
+ * it is a middleware we will apply to the lamda function
+ * when exporting it to avoid the usage of the JSON.parse
+ * to obtain the body params from the event
+ */
+const middy = require('@middy/core');
+const httpJsonBodyParser = require('@middy/http-json-body-parser');
 
-const addTodo = async (event) => {
+const addTodo = ({ _AWS, _v4 }) => async (event) => {
   /**
    * REMINDER, for this to work your AWS user for
    * serverless should get access to be able to
    * use the dynamoDb resource.
    */
-  const dynamo = new AWS.DynamoDB.DocumentClient() // We need to create an instance of the dynamoDB
+  const dynamo = new _AWS.DynamoDB.DocumentClient() // We need to create an instance of the dynamoDB
 
   /**
    * The event.body (where the body params
@@ -17,10 +25,10 @@ const addTodo = async (event) => {
    * instead of an object so we need to JSON
    * parse it.
    */
-  const { todo } = JSON.parse(event.body);
+  const { todo } = event.body;
   const createdAt = new Date();
   // Generate a unique id
-  const id = v4();
+  const id = _v4();
   console.log('This is an id', id);
 
   const newTodo = {
@@ -42,5 +50,10 @@ const addTodo = async (event) => {
 };
 
 module.exports = {
-  handler: addTodo,
+  /**
+   * This is how middy is used in order to automatically
+   * parse the body with this middleware
+   */
+  handler: middy(addTodo({ _AWS: AWS, _v4: v4 })).use(httpJsonBodyParser()),
+  addTodo
 };
